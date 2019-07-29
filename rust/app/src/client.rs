06f8ae68874135ce23fe6d1f12e49bd99a689464
Oleg Gashev <oleg@gashev.net>
2019-07-29 15:29:04 -0400

    Added client app.

---
+++ rust/app/src/client.rs
@@ -0,0 +1,24 @@
+use std::sync::Arc;
+
+use grpcio::{ChannelBuilder, EnvBuilder};
+
+#[path = "../../books.rs"]
+mod books;
+
+#[path = "../../books_grpc.rs"]
+mod books_grpc;
+
+use books_grpc::{create_books, Books, BooksClient};
+use books::{AddBookRequest, BookReply, GetBookRequest};
+
+fn main() {
+    let env = Arc::new(EnvBuilder::new().build());
+    let ch = ChannelBuilder::new(env).connect("localhost:50051");
+    let client = BooksClient::new(ch);
+
+    let mut req = AddBookRequest::default();
+    req.set_authors("author".to_owned());
+    req.set_title("title".to_owned());
+    let reply = client.add_book(&req).expect("rpc");
+    println!("received: {}", reply.get_id());
+}
