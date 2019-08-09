e5cfd591ebd62ba984097b385be132ab99023328
Oleg Gashev <oleg@gashev.net>
2019-08-09 08:01:10 -0400

    Added clap to cli and added add book flow.

--- rust/src/cli/src/main.rs
+++ rust/src/cli/src/main.rs
@@ -10,15 +10,43 @@ mod books_grpc;
 
 use books_grpc::BooksClient;
 use books::{AddBookRequest, BookReply};
- 
-fn main() {
-    let env = Arc::new(EnvBuilder::new().build());
-    let ch = ChannelBuilder::new(env).connect("server:50051");
-    let client = BooksClient::new(ch);
 
-    let mut req = AddBookRequest::default();
-    req.set_authors("author".to_owned());
-    req.set_title("title".to_owned());
-    let reply: BookReply = client.add_book(&req).expect("rpc");
-    println!("received: {}", reply.get_id());
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
+fn main() {
+    let matches = App::new("cli")
+        .subcommand(SubCommand::with_name("add")
+            .about("add book")
+            .arg(Arg::with_name("authors")
+                .short("a")
+                .long("authors")
+                .help("authors")
+                .required(true)
+                .takes_value(true))
+            .arg(Arg::with_name("title")
+                .short("t")
+                .long("title")
+                .help("title")
+                .required(true)
+                .takes_value(true))
+        )
+    .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("add") {
+        let authors = matches.value_of("authors").unwrap();
+        let title = matches.value_of("title").unwrap();
+
+        println!("{} {}", authors, title);
+
+        let env = Arc::new(EnvBuilder::new().build());
+        let ch = ChannelBuilder::new(env).connect("server:50051");
+        let client = BooksClient::new(ch);
+
+        let mut req = AddBookRequest::default();
+        req.set_authors(authors.to_owned());
+        req.set_title(title.to_owned());
+        let reply: BookReply = client.add_book(&req).expect("rpc");
+        println!("received: {}", reply.get_id());
+    }
 }
