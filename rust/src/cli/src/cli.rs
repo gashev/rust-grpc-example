d3e1a794bbebfdd53e838b26ec809ce3a83a3fad
Oleg Gashev <oleg@gashev.net>
2019-08-17 06:40:27 -0400

    Added to cli update and delete book commands.

--- rust/src/cli/src/cli.rs
+++ rust/src/cli/src/cli.rs
@@ -37,10 +37,48 @@ fn get_books_command() -> clap::App<'static, 'static> {
         .about("get all books");
 }
 
+fn update_book_command() -> clap::App<'static, 'static> {
+    return
+        SubCommand::with_name("update")
+        .about("update book")
+        .arg(Arg::with_name("id")
+            .short("i")
+            .long("id")
+            .help("id")
+            .required(true)
+            .takes_value(true))
+        .arg(Arg::with_name("authors")
+            .short("a")
+            .long("authors")
+            .help("authors")
+            .required(true)
+            .takes_value(true))
+        .arg(Arg::with_name("title")
+            .short("t")
+            .long("title")
+            .help("title")
+            .required(true)
+            .takes_value(true));
+}
+
+fn delete_book_command() -> clap::App<'static, 'static> {
+    return
+        SubCommand::with_name("delete")
+        .about("delete book")
+        .arg(Arg::with_name("id")
+            .short("i")
+            .long("id")
+            .help("id")
+            .required(true)
+            .takes_value(true));
+}
+
 pub fn get_cli() -> clap::App<'static, 'static> {
     return
         App::new("cli")
         .subcommand(add_book_command())
         .subcommand(get_book_command())
-        .subcommand(get_books_command());
+        .subcommand(get_books_command())
+        .subcommand(update_book_command())
+        .subcommand(delete_book_command());
 }
